d90c6ce7ac269692c97422a013a6cc30b170c4e9
Viet Nguyen <VIET.NGUYEN@iprospect.com>
2020-10-13 12:58:31 +0700

    make it better

--- routes/activity.js
+++ routes/activity.js
@@ -85,7 +85,7 @@ exports.execute = function (req, res) {
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
             
             // decoded in arguments
-            var decodedArgs = decoded.inArguments[0];
+            var decodedArgs = decoded.inArguments[2];
             var decodedArgs2 = decoded.inArguments[1];
             var decodedArgs3 = decoded.outArguments[1];
             // logData(req);
