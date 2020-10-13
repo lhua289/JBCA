f8feefc5c2b1494fd2a35a94ebd86e9ba910adeb
Viet--- <nghoangviet17@gmail.com>
2020-10-14 00:00:13 +0700

    make it better

--- app.js
+++ app.js
@@ -37,7 +37,39 @@ app.post('/logout', routes.logout );
 app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+app.post('/journeybuilder/execute/', function (req, res) {
+
+    // example on how to decode JWT
+    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
+
+        // verification error -> unauthorized request
+        if (err) {
+            console.error(err);
+            return res.status(401).end();
+        }
+
+        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+            
+            // decoded in arguments
+            var decodedArgs = decoded.inArguments[0];
+            var decodedArgs1 = decoded.outArguments[0];
+            // var decodedArgs2 = decoded.inArguments[0].contact.key;
+            // logData(req);
+            // res.send(200, 'Execute');
+            console.log('decodedArgs',decodedArgs);
+            console.log('decodedArgs1',decodedArgs1);
+            console.log("=======decoded-KEYVALUE========", decoded.keyValue);
+
+        } else {
+            console.error('inArguments invalid.');
+            return res.status(400).end();
+        }
+    });
+    
+    // console.log(req.body.length);
+    // console.log(JSON.stringify(req.body.length));
+    
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
