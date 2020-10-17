678833ab032d53c9ef2388e06b0ebfa4ccb6eb2f
Viet Nguyen <viet.nguyen@iprospect.com>
2020-10-17 16:40:46 +0700

    make it better

--- routes/activity.js
+++ routes/activity.js
@@ -65,8 +65,9 @@ exports.edit = function (req, res) {
 exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    // logData(req);
-    res.send(200, 'Save');
+    logData(req);
+    // res.send(200, 'Save');
+    
 
 };
 
