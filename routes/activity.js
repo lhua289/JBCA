7fd1613cb339a09c502d7ace1bb50da94d24e877
Viet--- <nghoangviet17@gmail.com>
2020-10-18 12:42:52 +0700

    make it better

--- routes/activity.js
+++ routes/activity.js
@@ -99,7 +99,7 @@ exports.execute = function (req, res) {
             var subStr = decodedArgs.match("%%(.*)%%");
             
             // if ( subStr[1] = "FirstName") {
-                var messText =   decodedArgs.replace(subStr[1], FirstName);
+                var messText =   (decodedArgs.replace(subStr[1], FirstName)).replace("%%", "");
             // }
             console.log('subStr',messText);
             // Call Zalo API
